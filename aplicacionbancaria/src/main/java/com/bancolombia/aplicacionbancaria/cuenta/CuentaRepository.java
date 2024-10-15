ecadd14a8b4bf5dcf9a6bd5d686308b7901f8b95
cesar villalobos <cesarvilla9421@gmail.com>
2024-10-15 15:39:48 -0500

    Desarrollo de taller

--- aplicacionbancaria/src/main/java/com/bancolombia/aplicacionbancaria/cuenta/CuentaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bancolombia.aplicacionbancaria.cuenta;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CuentaRepository extends JpaRepository<Cuenta, String> {
-}
--- aplicacionbancaria/src/main/java/com/bancolombia/aplicacionbancaria/transaccion/TransaccionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bancolombia.aplicacionbancaria.transaccion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransaccionRepository extends JpaRepository<Transaccion, Long> {
-}
